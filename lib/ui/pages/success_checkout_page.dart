be51f80bb5bee4d0fed93b8638624d80ad349980
embunazzahra <dhauembunazzahra@gmail.com>
2022-01-05 11:33:52 +0700

    adding success checkout page

--- lib/ui/pages/success_checkout_page.dart
+++ lib/ui/pages/success_checkout_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:travel_app/shared/theme.dart';
+import 'package:travel_app/ui/widgets/custom_button.dart';
 
 class SuccessCheckoutPage extends StatelessWidget {
   const SuccessCheckoutPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: kWhiteColor,
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                width: 300,
+                height: 150,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/icon_booked.png')))),
+            SizedBox(
+              height: 80,
+            ),
+            Text(
+              'Well Booked 😍',
+              style: blackFont.copyWith(fontSize: 32, fontWeight: semiBold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Are you ready to explore the new\nworld of experiences?',
+              style: greyFont.copyWith(fontSize: 16, fontWeight: light),
+              textAlign: TextAlign.center,
+            ),
+            CustomButton(
+              title: 'My Bookings',
+              onPressed: () {},
+              width: 220,
+              margin: EdgeInsets.only(top: 50),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
