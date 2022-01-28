c25c231fde245c66c4a3c4c5518915cf1d3398a5
embunazzahra <dhauembunazzahra@gmail.com>
2022-01-28 23:54:11 +0700

    sync the auth service & cubit with signUp

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -5,6 +5,20 @@ import 'package:travel_app/services/user_service.dart';
 class AuthService {
   FirebaseAuth _auth = FirebaseAuth.instance;
 
+  Future<UserModel> signIn(
+      {required String email, required String password}) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+
+      UserModel user =
+          await UserService().getUserById(userCredential.user!.uid);
+      return user;
+    } catch (e) {
+      throw e;
+    }
+  }
+
   Future<UserModel> signUp(
       {required String email,
       required String password,
